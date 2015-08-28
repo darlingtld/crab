589920970284a9297236ef6aee12dad7ba0cad6a
darlingtld <darlingtld@qq.com>
2015-08-28 13:48:58 +0800

    cardcode

---
+++ src/main/webapp/modules/card_expense/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by tangl9 on 2015-08-28.
+ */
+var mainModule = angular.module('MainModule', ['ngRoute']);
+
+mainModule.controller('expenseController', function ($scope, $http) {
+
+})
