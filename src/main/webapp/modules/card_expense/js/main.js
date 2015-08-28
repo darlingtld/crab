125e8f7e5d36d83b34ebfc856a4108b0365afa09
darlingtld <darlingtld@qq.com>
2015-08-28 15:16:22 +0800

    cardcode

--- src/main/webapp/modules/card_expense/js/main.js
+++ src/main/webapp/modules/card_expense/js/main.js
@@ -4,5 +4,25 @@
 var mainModule = angular.module('MainModule', ['ngRoute']);
 
 mainModule.controller('expenseController', function ($scope, $http) {
+    $scope.confirm = function () {
+        console.log($scope.cardcode);
+    };
+    $scope.cancel = function () {
+        $('#confirmDialog button.close').click();
+    };
+});
 
-})
+mainModule.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/expense', {
+            controller: 'expenseController',
+            templateUrl: 'expense.html'
+        })
+        .when('/delivery', {
+            controller: 'deliveryController',
+            templateUrl: 'delivery.html'
+        })
+        .otherwise({
+            redirectTo: '/expense'
+        });
+}]);
